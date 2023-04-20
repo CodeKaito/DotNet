163b1122562737b6e14afb1894f04403c7c67303
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-20 11:37:32 +0200

    update

---
+++ Myapp/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Myapp.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index(){
+            return View();
+        }
+
+        public IActionResult Detail(string id){
+            return Content($"Io sono Detail, ho ricevuto l'id {id}");
+        }
+    }
+}
--- Myapp/Startup.cs
+++ Myapp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Myapp
 {
@@ -15,6 +16,7 @@ namespace Myapp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +26,11 @@ namespace Myapp
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.UseMvc(routeBuilder =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

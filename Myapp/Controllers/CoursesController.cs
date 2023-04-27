ad318f3d3f1cbc97bdc3857aff22d9bfb788e27c
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-27 09:07:40 +0200

    update

--- Myapp/Controllers/CoursesController.cs
+++ Myapp/Controllers/CoursesController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Myapp.Course.ViewModels;
 
 namespace Myapp.Controllers
 {
     public class CoursesController : Controller
     {
-        public IActionResult Index(){
-            return View();
+        public IActionResult Index()
+        {
+            var courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetServices();
+            return View(courses);
         }
 
-        public IActionResult Detail(string id){
+        public IActionResult Detail(string id)
+        {
             return View();
         }
     }
